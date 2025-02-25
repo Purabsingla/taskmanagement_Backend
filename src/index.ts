f4af948883dec53549619514db19b1e35fab11ce
Purabsingla <purabsingla15@gmail.com>
2025-02-25 15:29:55 +0530

    First Commit

---
+++ src/index.ts
@@ -0,0 +1,15 @@
+import express, { Request, Response, Application } from "express";
+import cors from "cors";
+
+const app: Application = express();
+app.use(cors());
+
+app.get("/api/hello", (req: Request, res: Response) => {
+  res.json({ message: "Hello, World!" });
+});
+console.log("Hello World~");
+
+app.listen(8080, (error) => {
+  if (error) console.log(error);
+  else console.log("Server is running on port 8080");
+});
