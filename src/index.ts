dbca128b3d4cd7eb76b185e9190e23a07adad4df
Purabsingla <purabsingla15@gmail.com>
2025-02-26 15:54:52 +0530

    Add nodemon configuration and update express middleware

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,7 @@ import cors from "cors";
 
 const app: Application = express();
 app.use(cors());
+app.use(express.json());
 
 app.get("/api/hello", (req: Request, res: Response) => {
   res.json({ message: "Hello, World!" });
