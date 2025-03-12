481fd3698d8536ca5490df390ee57d10142b4afe
Purabsingla <purabsingla15@gmail.com>
2025-03-12 19:59:16 +0530

    Hash user passwords during signup and update user details saving logic

--- src/Controller/AuthController.ts
+++ src/Controller/AuthController.ts
@@ -1,10 +1,17 @@
 import { Request, Response } from "express";
 import { db, admin } from "../Database/FireBase";
+import bycrypt from "bcryptjs";
 
 const signup = async (req: Request, res: Response) => {
   const { email, password } = req.body;
   try {
-    const userData = await admin.auth().createUser({ email, password });
+    const HashedPassword = await bycrypt.hash(password, 10);
+    const userData = await admin.auth().createUser({ email });
+    await db.collection("users").doc(userData.uid).set({
+      email,
+      password: HashedPassword,
+    });
+
     res.status(200).send({
       message: "User created successfully",
       uid: userData.uid,
@@ -17,14 +24,16 @@ const signup = async (req: Request, res: Response) => {
 };
 
 const signupdetails = async (req: Request, res: Response) => {
-  const { uid, name, email, phone } = req.body;
+  const { uid, name, phone } = req.body;
   try {
-    await db.collection("users").doc(uid).set({
-      name,
-      email,
-      phone,
-      createdAt: new Date(),
-    });
+    await db.collection("users").doc(uid).set(
+      {
+        name,
+        phone,
+        createdAt: new Date(),
+      },
+      { merge: true }
+    );
     res.status(200).send({
       message: "User details saved successfully",
     });
